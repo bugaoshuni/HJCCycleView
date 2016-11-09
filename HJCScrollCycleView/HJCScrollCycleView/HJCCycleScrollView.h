92a681995c4d6c97ae62df50923647912754e46c
bugaoshuni <1421609378@qq.com>
2016-11-09 18:32:25 +0800

    no message

---
+++ HJCScrollCycleView/HJCScrollCycleView/HJCCycleScrollView.h
@@ -0,0 +1,13 @@
+//
+//  HJCCycleScrollView.h
+//  HJCScrollCycleView
+//
+//  Created by jichanghe on 2016/11/9.
+//  Copyright © 2016年 hjc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HJCCycleScrollView : UIView
+
+@end
---
+++ HJCScrollCycleView/HJCScrollCycleView/HJCCycleScrollView.m
@@ -0,0 +1,20 @@
+//
+//  HJCCycleScrollView.m
+//  HJCScrollCycleView
+//
+//  Created by jichanghe on 2016/11/9.
+//  Copyright © 2016年 hjc. All rights reserved.
+//
+
+#import "HJCCycleScrollView.h"
+
+@implementation HJCCycleScrollView
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        
+    }
+    return self;
+}
+
+@end
